da1bbe537a3f9a8893e6ed85886d26f48005933d
Michał <mtrela2222@gmail.com>
2026-01-03 13:33:35 +0100

    Add Weather class to weather_api.py and import it in main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+from weather_api import Weather
--- weather_api.py
+++ weather_api.py
@@ -1,1 +1,29 @@
+import json
 import requests
+import config
+
+
+class Weather:
+    def __init__(self):
+        self.api_key = config.API_KEY
+        self.base_url = config.BASE_URL
+
+    def _build_params(self,city):
+        params = {
+            "q": city,
+            "appid": self.api_key,
+            "units": "metric",
+            "lang": "pl"
+        }
+        return params
+
+    def get_weather(self,city):
+        params = self._build_params(city)
+        response = requests.get(self.base_url,params=params)
+
+        try:
+            if response.status_code == 200:
+                return response.json()
+
+        except Exception:
+            print(f"Blad, kod:{response.status_code}")
